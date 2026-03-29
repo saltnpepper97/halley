d415fd9d9bf7c4c006aa7d442176ddae9a4755c2
Dustin Pilgrim <dustin.pilgrim1997@gmail.com>
2026-03-29 15:28:55 -0300

    refactor(wayland): moved wayland related code to protocol/

--- crates/halley-wl/src/lib.rs
+++ crates/halley-wl/src/lib.rs
@@ -7,10 +7,10 @@ pub(crate) mod input;
 pub(crate) mod interaction;
 pub(crate) mod ipc;
 pub(crate) mod overlay;
+pub(crate) mod protocol;
 pub mod render;
 pub(crate) mod spatial;
 pub mod state;
-pub(crate) mod wayland;
 pub(crate) mod wm;
 
 pub use bootstrap::{run, run_winit};
